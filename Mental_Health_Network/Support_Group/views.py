2cc6f1df23e20b7c8881aa6e059f07765c428b55
Salman-data-cloud <160743157+Salman-data-cloud@users.noreply.github.com>
2024-02-28 08:24:27 +0600

    Update views.py

--- Mental_Health_Network/Support_Group/views.py
+++ Mental_Health_Network/Support_Group/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from .models import *
 
+def support_app(request):
+    context = {'page': 'Home'}
+    data = [
+        {'group_name': 'Technical Issues1', 'group_id': 1},
+        {'group_name': 'Technical Issues2', 'group_id': 2},
+        {'group_name': 'Appointment Issues1', 'group_id': 3},
+        {'group_name': 'Appointment Issues2', 'group_id': 4},
+        {'group_name': 'Payment Issues', 'group_id': 5}
+    ]
+    return render(request, 'support_group_list.html', context={'page': 'Home', 'data': data})
 
-# Create your views here.
+def support_detail(request, group_id):
+    support_group = get_object_or_404(SupportGroup, id=group_id)
+    messages = Message.objects.filter(support_group=support_group)
+    context = {'group': support_group, 'messages': messages}
+    return render(request, 'support_group_detail.html', context
 
 
 
