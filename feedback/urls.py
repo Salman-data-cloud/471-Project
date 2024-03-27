9ffd42f37c18b76361b2d082274c8ba49168bc61
Salman-data-cloud <salmanpasha3011@gmail.com>
2024-03-27 11:17:35 +0600

    Moved

--- feedback/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from feedback.views import *
-
-urlpatterns = [
-    path('', feedback_view, name='feedback'),
-    path('success/<int:feedback_id>/', feedback_success, name='feedback_success'),
-]
