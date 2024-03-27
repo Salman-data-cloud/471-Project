b96b8e18d9c7b95caf0827ffd42254dfb8ea4710
ridwan <kaif00056@gmail.com>
2024-03-27 06:16:21 +0600

    created appointment scheduling and more

--- Mental_Health_Network/Mental_Health_Network/settings.py
+++ Mental_Health_Network/Mental_Health_Network/settings.py
@@ -136,3 +136,14 @@ AUTH_USER_MODEL = 'Login_Authentication.UserLoginAuth'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "kaif00056@gmail.com"
+#dohk noev xxzi axyy
+EMAIL_HOST_PASSWORD = "dohknoevxxziaxyy"
+
