7d14dfddabe14e3fa1419557494d9627635035d0
SamantaBinte <97800431+SamantaBinte@users.noreply.github.com>
2024-04-23 21:13:08 +0600

    Delete chatbot directory

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', call_chatbot, name='call_chatbot'),
-    path('chat/', chat_response, name='chat_response'),  # Adjusted to match your JS fetch call
-]
